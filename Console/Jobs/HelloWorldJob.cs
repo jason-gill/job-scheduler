39010538151d97802099159a317cc4f17b8ad22d
MARKIT\jason.gill <jason.gill@ihsmarkit.com>
2017-06-26 08:29:59 -0500

    Got the job sequence to work

--- Console/Jobs/HelloWorldJob.cs
+++ Console/Jobs/HelloWorldJob.cs
@@ -7,40 +7,27 @@ namespace Console.Jobs
         public const string IdentityId = "IdentityId";
 
         private static int _count;
-        private static bool _executedSuccessfully;
         
         public void Execute(IJobExecutionContext context)
         {
+            var isLastExecution = !context.NextFireTimeUtc.HasValue;
             var data = context.JobDetail.JobDataMap;
             var identityId = data.GetIntValue(IdentityId);
-            var finalized = data.GetBooleanValue(SchedulerListener.Finalized);
 
-            if (_executedSuccessfully)
-            {
-                System.Console.WriteLine($"{System.DateTime.Now:r}: Job with IdenittyId: {identityId} already executed successfully");
-                
-                // If the job executed successfully the last time we
-                // don't need to run it again
-                var e2 = new JobExecutionException {UnscheduleAllTriggers = true};
-                throw e2;
-            }
-            
-            if (finalized)
-            {
-                System.Console.WriteLine($"{System.DateTime.Now:r}: Finalizing job with IdenittyId: {identityId}");
-                var e2 = new JobExecutionException {UnscheduleAllTriggers = true};
-                throw e2;
-            }
-            
             System.Console.WriteLine($"{System.DateTime.Now:r}: Executing Job with IdentityId: {identityId}");
 
             _count++;
-            if (_count == 99)
+            // If job was successful
+            if (_count == 2)
             {
-                _executedSuccessfully = true;
                 System.Console.WriteLine("Job executed successfully!");
-                var e2 = new JobExecutionException {UnscheduleAllTriggers = true};
-                throw e2;
+                var stopExecuting = new JobExecutionException {UnscheduleAllTriggers = true};
+                throw stopExecuting;
+            }
+
+            if (isLastExecution)
+            {
+                System.Console.WriteLine($"{System.DateTime.Now:r}: Finalizing job with IdenittyId: {identityId}");
             }
         }
     }
--- Console/Scheduler.cs
+++ Console/Scheduler.cs
@@ -19,7 +19,6 @@ namespace Console
                     {
                         ISchedulerFactory sf = new StdSchedulerFactory();
                         _instance = sf.GetScheduler();
-                        _instance.ListenerManager.AddSchedulerListener(new SchedulerListener());
                         _instance.Start();
                     }
                 }
@@ -27,115 +26,5 @@ namespace Console
  
             return _instance;
         }
-    }
-
-    public class SchedulerListener : ISchedulerListener
-    {
-        public const string Finalized = "Finalized";
-        
-        public void TriggerFinalized(ITrigger trigger)
-        {
-            var scheduler = Scheduler.GetInstance();
-            
-            var jobKey = trigger.JobKey;
-            var currentJob = scheduler.GetJobDetail(jobKey);
-            
-            var jobDataMap = new JobDataMap(currentJob.JobDataMap.WrappedMap);
-            jobDataMap.Put(Finalized, true);
-
-            var finalJob = JobBuilder.Create(currentJob.JobType)
-                .WithIdentity(jobKey.Name, "Finalized")
-                .SetJobData(jobDataMap)
-                .Build();
-            
-            var triggerOnce = TriggerBuilder.Create()
-                .StartNow()
-                .WithSimpleSchedule(x => x
-                    .WithRepeatCount(0))
-                .Build();
-
-            System.Console.WriteLine($"TriggerFinalized - Scheduling Job: {jobKey.Name}");
-            scheduler.ScheduleJob(finalJob, triggerOnce);
-        }
-
-        #region Not Implemented
-
-        public void JobScheduled(ITrigger trigger)
-        {
-        }
-
-        public void JobUnscheduled(TriggerKey triggerKey)
-        {
-        }
-
-        public void TriggerPaused(TriggerKey triggerKey)
-        {
-        }
-
-        public void TriggersPaused(string triggerGroup)
-        {
-        }
-
-        public void TriggerResumed(TriggerKey triggerKey)
-        {
-        }
-
-        public void TriggersResumed(string triggerGroup)
-        {
-        }
-
-        public void JobAdded(IJobDetail jobDetail)
-        {
-        }
-
-        public void JobDeleted(JobKey jobKey)
-        {
-        }
-
-        public void JobPaused(JobKey jobKey)
-        {
-        }
-
-        public void JobsPaused(string jobGroup)
-        {
-        }
-
-        public void JobResumed(JobKey jobKey)
-        {
-        }
-
-        public void JobsResumed(string jobGroup)
-        {
-        }
-
-        public void SchedulerError(string msg, SchedulerException cause)
-        {
-        }
-
-        public void SchedulerInStandbyMode()
-        {
-        }
-
-        public void SchedulerStarted()
-        {
-        }
-
-        public void SchedulerStarting()
-        {
-        }
-
-        public void SchedulerShutdown()
-        {
-        }
-
-        public void SchedulerShuttingdown()
-        {
-        }
-
-        public void SchedulingDataCleared()
-        {
-        }
-        
-        #endregion
     }
 }
