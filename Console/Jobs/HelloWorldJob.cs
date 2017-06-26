06e9ee899723d0f16c3d3f53c8799c1c16d50b8e
Jason Gill <jason@thegillhousehold.com>
2017-06-25 22:44:25 -0500

    Initial commit

---
+++ Console/Jobs/HelloWorldJob.cs
@@ -0,0 +1,47 @@
+﻿using Quartz;
+
+namespace Console.Jobs
+{
+    public class HelloWorldJob : IJob
+    {
+        public const string IdentityId = "IdentityId";
+
+        private static int _count;
+        private static bool _executedSuccessfully;
+        
+        public void Execute(IJobExecutionContext context)
+        {
+            var data = context.JobDetail.JobDataMap;
+            var identityId = data.GetIntValue(IdentityId);
+            var finalized = data.GetBooleanValue(SchedulerListener.Finalized);
+
+            if (_executedSuccessfully)
+            {
+                System.Console.WriteLine($"{System.DateTime.Now:r}: Job with IdenittyId: {identityId} already executed successfully");
+                
+                // If the job executed successfully the last time we
+                // don't need to run it again
+                var e2 = new JobExecutionException {UnscheduleAllTriggers = true};
+                throw e2;
+            }
+            
+            if (finalized)
+            {
+                System.Console.WriteLine($"{System.DateTime.Now:r}: Finalizing job with IdenittyId: {identityId}");
+                var e2 = new JobExecutionException {UnscheduleAllTriggers = true};
+                throw e2;
+            }
+            
+            System.Console.WriteLine($"{System.DateTime.Now:r}: Executing Job with IdentityId: {identityId}");
+
+            _count++;
+            if (_count == 99)
+            {
+                _executedSuccessfully = true;
+                System.Console.WriteLine("Job executed successfully!");
+                var e2 = new JobExecutionException {UnscheduleAllTriggers = true};
+                throw e2;
+            }
+        }
+    }
+}
---
+++ Console/Program.cs
@@ -0,0 +1,36 @@
+﻿using Console.Jobs;
+using Quartz;
+
+namespace Console
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Starting Scheduler...");
+           
+            Scheduler.GetInstance();
+            
+            var job = JobBuilder.Create<HelloWorldJob>()
+                .WithIdentity("myJob", "group1")
+                .UsingJobData(HelloWorldJob.IdentityId, 7)
+                .Build();
+		
+            var trigger = TriggerBuilder.Create()
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInSeconds(5)
+                    .WithRepeatCount(2))
+                .Build();
+		
+            // Tell quartz to schedule the job using our trigger
+            Scheduler.GetInstance().ScheduleJob(job, trigger);
+            
+            System.Console.WriteLine("Press any key to quit.");
+            System.Console.ReadKey();
+            
+            Scheduler.GetInstance().Shutdown(true);
+            System.Console.WriteLine("Scheduler Shutdown...");
+        }
+    }
+}
